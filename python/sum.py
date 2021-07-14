c7d6c004bf733d46958dc7334800ec967ef9d449
satish-rajnale <satishrajnale98@gmail.com>
2021-07-14 16:45:08 +0530

    twosum

--- python/sum.py
+++ python/sum.py
@@ -32,3 +32,22 @@ def add_it_up(n):
     except TypeError:
         result = 0
     return result
+
+
+# Two sum in python
+class Solution(object):
+   def twoSum(self, nums, target):
+      """
+      :type nums: List[int]
+      :type target: int
+      :rtype: List[int]
+      """
+      required = {}
+      for i in range(len(nums)):
+         if target - nums[i] in required:
+            return [required[target - nums[i]],i]
+         else:
+            required[nums[i]]=i
+input_list = [2,8,12,15]
+ob1 = Solution()
+print(ob1.twoSum(input_list, 20))

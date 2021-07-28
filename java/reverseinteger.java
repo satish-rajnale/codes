c3192b928703377d4bd5ba56cbd2252ca4ba440c
satish-rajnale <satishrajnale98@gmail.com>
2021-07-28 22:31:40 +0530

    palindrome

--- java/reverseinteger.java
+++ java/reverseinteger.java
@@ -12,3 +12,22 @@ class Solution {
         return y;
     }
 }
+
+
+class Solution {
+    public boolean isPalindrome(int x) {
+         // Base condition
+        if (x < 0) {
+            return false;
+        }
+        // Store the number in a variable
+        int number = x;
+        // This will store the reverse of the number
+        int reverse = 0;
+        while (number > 0) {
+            reverse = reverse * 10 + number % 10;
+            number /= 10;
+        }
+        return x == reverse;
+    }
+}

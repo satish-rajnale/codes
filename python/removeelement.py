e05abd1251323b0f60a4a2547d8a31c3b45be007
satish-rajnale <satishrajnale98@gmail.com>
2021-08-05 22:54:18 +0530

    Create removeelement.py

---
+++ python/removeelement.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+       
+        # Counter for keeping track of elements other than val
+        count = 0
+        # Loop through all the elements of the array
+        for i in range(len(nums)):
+            if nums[i] != val:
+                # If the element is not val
+                nums[count] = nums[i]
+                count += 1
+        return count

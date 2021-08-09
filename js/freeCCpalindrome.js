36eb1eb90f1dd41841685ce439afe0090ab6296e
satish-rajnale <satishrajnale98@gmail.com>
2021-08-09 21:45:12 +0530

    Create freeCCpalindrome.js

---
+++ js/freeCCpalindrome.js
@@ -0,0 +1,11 @@
+function palindrome(str) {
+    let onlyLetters = str.replace(/[`~ !@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+    onlyLetters = onlyLetters.toLowerCase().split("");
+    for (let i = 0; i < onlyLetters.length - 1 / 2; i++) {
+      if (onlyLetters[i] !== onlyLetters[onlyLetters.length - i - 1]) {
+      return false;
+      break;
+    }
+  }
+  return true;
+}

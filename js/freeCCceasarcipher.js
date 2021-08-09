436ed4359c4ca8ff54589bdcc4d409061430fd4a
satish-rajnale <satishrajnale98@gmail.com>
2021-08-09 22:12:54 +0530

    Create freeCCceasarcipher.js

---
+++ js/freeCCceasarcipher.js
@@ -0,0 +1,10 @@
+function rot13(str) {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return str
+      .split('')
+      .map(char => {  
+        const pos = alphabet.indexOf(char);      
+        return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
+      })
+      .join('');
+}

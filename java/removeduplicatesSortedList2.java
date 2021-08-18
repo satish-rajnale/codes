b1bafe3316cc5525e930c13b4668038ccf024a56
satish-rajnale <satishrajnale98@gmail.com>
2021-08-18 20:37:28 +0530

    Create removeduplicatesSortedList2.java

---
+++ java/removeduplicatesSortedList2.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+         ListNode t = new ListNode(0);
+    t.next = head;
+ 
+    ListNode p = t;
+    while(p.next!=null&&p.next.next!=null){
+        if(p.next.val == p.next.next.val){
+            int dup = p.next.val;
+            while(p.next!=null&&p.next.val==dup){
+                p.next = p.next.next;
+            }
+        }else{
+            p=p.next;
+        }
+ 
+    }
+ 
+    return t.next;
+    }
+}

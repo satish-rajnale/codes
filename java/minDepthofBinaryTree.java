bd4a3d0e497b94944fd411b6de7a3fdb325bab66
satish-rajnale <satishrajnale98@gmail.com>
2021-08-24 20:17:02 +0530

    Create minDepthofBinaryTree.java

---
+++ java/minDepthofBinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+     public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        return findMinDepth(root);
+    }
+    
+    private int findMinDepth(TreeNode root) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+                
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+        
+        return Math.min(findMinDepth(root.left), findMinDepth(root.right)) + 1;
+    }
+}

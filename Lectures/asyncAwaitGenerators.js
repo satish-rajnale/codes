e5fc4c3d7401e5da6b29ca2d73bd8347c5d934b6
satish-rajnale <satishrajnale98@gmail.com>
2021-09-03 20:33:30 +0530

    Update asyncAwaitGenerators.js

--- Lectures/asyncAwaitGenerators.js
+++ Lectures/asyncAwaitGenerators.js
@@ -3,15 +3,24 @@ function dowhenDataRecieved(val){
 }
 
 function *createFlow(){
-    // const mypromise = new Promise();
+    
     let arr = new Promise((resolve, reject) => {
  setTimeout(function(){resolve("i got it")}, 3000);
-                                });
+});
     const getdata = yield arr
     console.log(getdata);
 }
 
 const returnNextElement = createFlow();
-const futureData = returnNextElement.next().value;
-console.log("its future :",futureData)
-futureData.then(dowhenDataRecieved).catch(reason => console.log(reason));
+const futureData = returnNextElement.next();
+console.log("its future :",futureData);
+futureData.value.then(dowhenDataRecieved).catch(reason => console.log(reason));
+
+function calcSomething(arr){
+    for(i=0;i<100000000;i++){
+
+    }
+    return arr.reduce((a,b) => a+b);
+}
+console.log("JUst checking that promise did not resolve yet!!!")
+console.log(calcSomething([1,2,3,4,5,6,7,8,9]))

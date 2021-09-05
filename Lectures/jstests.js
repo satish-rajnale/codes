fc19abae0e08743c8b89916d524da1b87592ef4d
satish-rajnale <satishrajnale98@gmail.com>
2021-09-05 20:05:10 +0530

    Create jstests.js

---
+++ Lectures/jstests.js
@@ -0,0 +1,17 @@
+function Starter (props, children){
+    this.children = children;
+    //return "initialized"+ (props && children && props.toString() +children.toString()) || "attach";
+};
+// Starter.filter = (x) => {
+//  return x*2;
+// }
+Starter.prototype.getsome = function() {
+   console.log(this.children)
+}
+const per = new Starter("12",555);
+
+per.getsome()
+
+
+const isITrue = Boolean( 1*2 == 3 && 7/2 == 3.5);
+console.log(isITrue)

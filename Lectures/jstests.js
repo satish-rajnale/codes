8114bfa30e19d80bb58531a1742743bfb0906a1a
satish-rajnale <satishrajnale98@gmail.com>
2021-09-15 20:14:49 +0530

    Update jstests.js

--- Lectures/jstests.js
+++ Lectures/jstests.js
@@ -1,17 +1,24 @@
-function Starter (props, children){
-    this.children = children;
-    //return "initialized"+ (props && children && props.toString() +children.toString()) || "attach";
-};
-// Starter.filter = (x) => {
-//  return x*2;
+// function Starter (props, children){
+//     this.children = children;
+//     //return "initialized"+ (props && children && props.toString() +children.toString()) || "attach";
+// };
+// // Starter.filter = (x) => {
+// //  return x*2;
+// // }
+// Starter.prototype.getsome = function() {
+//    console.log(this.children)
 // }
-Starter.prototype.getsome = function() {
-   console.log(this.children)
-}
-const per = new Starter("12",555);
+// const per = new Starter("12",555);
 
-per.getsome()
+// per.getsome()
 
 
-const isITrue = Boolean( 1*2 == 3 && 7/2 == 3.5);
+(function(){
+    // console.log(innerIife);
+    var innerIife = "something!!!!!!!!!!!!!!!!!";
+    console.log(isITrue);
+})()
+
+var isITrue = Boolean( 1*2 == 3 && 7/2 == 3.5);
 console.log(isITrue)
+

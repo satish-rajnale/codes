80617f9036e819858585bc5d16e4fd7c6f9f7187
satish-rajnale <satishrajnale98@gmail.com>
2021-09-19 22:32:32 +0530

    async comments

--- Lectures/asyncAwait.js
+++ Lectures/asyncAwait.js
@@ -1,9 +1,14 @@
 async function  createFlow(){
     console.log("me first!!");
+    //await has no effect on type of this fucntion
     const getdata = await (function(){ let ans = "nothing";for(let i=0;i<700000000;i++){}return ans = "Got ran after a long time.."})()
     console.log(getdata);
 }
 
 createFlow();
 console.log("im second!!");
+// output -->
+// me first!!
+// im second!!
+// Got ran after a long time..
 

8c8a82a2d24175d3165e299f61c49f7876b26eab
satish-rajnale <satishrajnale98@gmail.com>
2021-09-20 23:44:20 +0530

    promises

--- Lectures/promises.js
+++ Lectures/promises.js
@@ -1,20 +1,33 @@
 const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve('foo');
-    }, 300);
+  setTimeout(() => {
+    resolve("foo");
+  }, 300);
+});
+
+myPromise
+  .then((value) => {
+    return value + " and bar";
+  })
+  .then((value) => {
+    return value + " and bar again";
+  })
+  .then((value) => {
+    return value + " and again";
+  })
+  .then((value) => {
+    return value + " and again";
+  })
+  .then((value) => {
+    console.log(value);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-  myPromise
-.then(value => { return value + ' and bar'; })
-.then(value => { return value + ' and bar again'; })
-.then(value => { return value + ' and again'; })
-.then(value => { return value + ' and again'; })
-.then(value => { console.log(value) })
-.catch(err => { console.log(err) });
 // Promise.prototype.catch =(str)=>{return str};
-const presolved = Promise.resolve(()=> "i got resolved!!");
-const prejected = Promise.reject(()=> "i got rejected!!").catch((err)=> console.log("i got error"));
+const presolved = Promise.resolve(() => "i got resolved!!");
+const prejected = Promise.reject(() => "i got rejected!!").catch((err) =>
+  console.log("i got error")
+);
 
-
-presolved.then(val => console.log(val));
+presolved.then((val) => console.log(val));
 console.log(prejected);

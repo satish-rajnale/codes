e083dcff3d873e8d9226afd8033ea9c3203424ba
satish-rajnale <satishrajnale98@gmail.com>
2021-12-26 20:25:26 +0530

    Create Builder.js

---
+++ Lectures/Patterns/Builder.js
@@ -0,0 +1,93 @@
+// The Builder pattern allows a client to construct a complex object by specifying the type
+// and content only. Construction details are hidden from the client entirely.
+
+// The objects participating in this pattern are:
+
+// Director -- In example code: Shop
+// constructs products by using the Builder's multistep interface
+
+// Builder -- not used in JavaScript
+// declares a multistep interface for creating a complex product
+
+// ConcreteBuilder -- In example code: CarBuilder, TruckBuilder
+// implements the multistep Builder interface
+// maintains the product through the assembly process
+// offers the ability to retrieve the newly created product
+
+// Products -- In example code: Car, Truck
+// represents the complex objects being assembled
+
+function Shop() {
+  this.construct = function (builder) {
+    builder.step1();
+    builder.step2();
+    return builder.get();
+  };
+}
+
+function CarBuilder() {
+  this.car = null;
+
+  this.step1 = function () {
+    this.car = new Car();
+  };
+
+  this.step2 = function () {
+    this.car.addParts();
+  };
+
+  this.get = function () {
+    return this.car;
+  };
+}
+
+function TruckBuilder() {
+  this.truck = null;
+
+  this.step1 = function () {
+    this.truck = new Truck();
+  };
+
+  this.step2 = function () {
+    this.truck.addParts();
+  };
+
+  this.get = function () {
+    return this.truck;
+  };
+}
+
+function Car() {
+  this.doors = 0;
+
+  this.addParts = function () {
+    this.doors = 4;
+  };
+
+  this.say = function () {
+    console.log('I am a ' + this.doors + '-door car');
+  };
+}
+
+function Truck() {
+  this.doors = 0;
+
+  this.addParts = function () {
+    this.doors = 2;
+  };
+
+  this.say = function () {
+    console.log('I am a ' + this.doors + '-door truck');
+  };
+}
+
+function run() {
+  var shop = new Shop();
+  var carBuilder = new CarBuilder();
+  var truckBuilder = new TruckBuilder();
+  var car = shop.construct(carBuilder);
+  var truck = shop.construct(truckBuilder);
+
+  car.say();
+  truck.say();
+}
